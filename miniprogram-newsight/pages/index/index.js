403c6f553406e243d106401b69fb3c40a5feadb9
Randolph Liu <Randolph.Liu@vulcansteel.co.nz>
2020-09-16 14:22:34 +1200

    course finished and code tidy up

--- miniprogram-newsight/pages/index/index.js
+++ miniprogram-newsight/pages/index/index.js
@@ -1,65 +1,81 @@
+import common from "../../utils/public.js";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    dataList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    wx.request({
+      url: 'https://ku.qingnian8.com/school/list.php',
+      data: {
+        num: 5
+      },
+      success: res => {
+        res.data.forEach(item => {
+          item.posttime = common.getMyData(item.posttime, "Y-m-d")
+          item.title = common.getStrLen(item.title, 25)
+        })
+        this.setData({
+          dataList: res.data
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
